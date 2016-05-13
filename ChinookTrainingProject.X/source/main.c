92ad378864ce3eb6daf01f85d1453ae68aaa4bad
FredericChasse <Chasse.frederic@gmail.com>
2016-05-13 19:54:22 -0400

    Added lines

--- ChinookTrainingProject.X/source/main.c
+++ ChinookTrainingProject.X/source/main.c
@@ -57,6 +57,8 @@
 //            available, type "Uart." or "Port." and a description with
 //            examples of all functions will appear on screen.
 //
+//            DO NOT USE THE FILES "StateMachine.c/.h".
+//
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 //
 // Solution : The solution of this lesson is in the branch 
